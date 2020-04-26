ad64e790bf5dec3596e4adf54e98cf570d2f69d2
Gil <53229892+Gil-nuriel@users.noreply.github.com>
2020-04-26 12:33:24 +0300

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -3,7 +3,6 @@ public class HellowWorld {
 	public static void main(String[] args){
 		System.out.println("Hello From Git!");
 		System.out.println("Changes made");
-		System.out.println("Example for rami");
 
 	}
 }
